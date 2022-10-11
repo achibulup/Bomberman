edb3f0c3913d7e16a8b7cfbd70b8986ffd4f11f1
achibulup <achibulup@gmail.com>
2022-10-12 01:31:12 +0700

    modified module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -9,4 +9,5 @@ module com.uet.int2204.group2 {
     exports com.uet.int2204.group2.controller;
     exports com.uet.int2204.group2.entity;
     exports com.uet.int2204.group2.graphics;
+    exports com.uet.int2204.group2.utils;
 }
