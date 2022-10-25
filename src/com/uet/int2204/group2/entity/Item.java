cb069faab8291c084da6a01608b10134ffe85b46
achibulup <achibulup@gmail.com>
2022-10-26 01:01:22 +0700

    fixed item

--- src/com/uet/int2204/group2/entity/Item.java
+++ src/com/uet/int2204/group2/entity/Item.java
@@ -37,7 +37,9 @@ public abstract class Item extends Tile implements DestroyableTile {
 
   @Override
   public void destroy() {
-    this.explosionAnimation = new Animation(ResourceManager.itemExplosion);
+    if (!beingDestroyed()) {
+      this.explosionAnimation = new Animation(ResourceManager.itemExplosion);
+    }
   }
 
   public abstract void onCollect(Player player);
